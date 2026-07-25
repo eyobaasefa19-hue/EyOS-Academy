92fa90c38caefaa07cd4d317bdccd213dba09f4d
Eyob Asefa <eyobaasefa19@gmail.com>
2026-07-25 20:28:08 +0300

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,24 @@
 import { PrismaClient } from '@prisma/client';
 
+/**
+ * Creates a single instance of PrismaClient.
+ * This prevents creating multiple connections during Next.js Hot Module Replacement (HMR) in development.
+ */
 const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
 declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+  // eslint-disable-next-line no-var
+  var prismaGlobal: PrismaClientSingleton | undefined;
 }
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export default prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prismaGlobal = prisma;
+}
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
+export default prisma;
